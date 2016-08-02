deeccc1fafed890c3538c93df242b3326f0f6186
ttoinou <tit_toinou@hotmail.com>
2016-08-02 23:09:31 +0200

    findDivisionNumber and divisionTest mode

--- generate.py
+++ generate.py
@@ -3,13 +3,15 @@ import argparse
 from PIL import Image, ImageOps
 import time
 import os
+import math
+import random
 
 import chainer
 from chainer import cuda, Variable, serializers
 from net import *
 
 parser = argparse.ArgumentParser(description='Real-time style transfer image generator')
-parser.add_argument('input')
+parser.add_argument('mode', default='', type=str)
 parser.add_argument('--input', '-i', type=str)
 parser.add_argument('--gpu', '-g', default=0, type=int,
                     help='GPU ID (negative value indicates CPU)')
@@ -19,7 +21,25 @@ parser.add_argument('--scale', '-s', default=1.0, type=float)
 parser.add_argument('--border', '-b', default=0, type=int)
 args = parser.parse_args()
 
-
+def divisionNumberInequality(Mpx,w,h,o,n):
+    return (w/float(n)+2*o)*(h/float(n)+2*o) - Mpx*1000*1000
+'''
+# not working
+# wrong formula
+def findDivisionNumber0(pixelmax,width,height,overlap):
+    r = width/height + height/width - 2.0
+    S = height*width
+    o = overlap
+    Max = math.sqrt( (o*o*r + 4*pixelmax*1000*1000)/S ) -o*(1.0/width + 1.0/height)
+    
+    return int(math.ceil( 1.0 / Max ) )
+'''
+def findDivisionNumber(Mpx,w,h,o):
+    n = 1
+    while divisionNumberInequality(Mpx,w,h,o,n) > 0:
+        n += 1
+    return n
+    
 def restartTime():
     return time.time()
 
@@ -116,16 +136,17 @@ def generateFromImageUrl(args,inputUrl,outputUrl):
     showTime('with image Ops \t',start)
 
 # loading
-start = restartTime()
-
-model = FastStyleNet()
-serializers.load_npz(args.model, model)
-if args.gpu >= 0:
-    cuda.get_device(args.gpu).use()
-    model.to_gpu()
-xp = np if args.gpu < 0 else cuda.cupy
-
-showTime('loading\t\t',start)
+def loading(args):
+    start = restartTime()
+    
+    model = FastStyleNet()
+    serializers.load_npz(args.model, model)
+    if args.gpu >= 0:
+        cuda.get_device(args.gpu).use()
+        model.to_gpu()
+    xp = np if args.gpu < 0 else cuda.cupy
+    
+    showTime('loading\t\t',start)
 
 
 def main(args):
@@ -146,5 +167,26 @@ def main(args):
         generateFromImageUrl(args,args.input,args.out)
         
 
-if __name__ == '__main__':
+if args.mode == 'divisionTest':
+    j = 0
+    while True:
+        o = random.uniform(1,30)
+        w = random.uniform(31,4000)
+        h = random.uniform(31,4000)
+        M = random.uniform(0.1,2)
+        
+        n = findDivisionNumber(M,w,h,o)
+        if n > 1 and ( divisionNumberInequality(M,w,h,o,n) > 0 or divisionNumberInequality(M,w,h,o,n-1) < 0 ) :
+            print "division test error",o,w,h,M,n
+        else:
+            j += 1
+            
+        if j % 1000000 == 0:
+            print "division test no error",j
+        
+        
+    
+elif args.mode == '' or __name__ == '__main__':
+    loading(args)
     main(args)
+    
